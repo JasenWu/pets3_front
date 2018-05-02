68359635d701cb2ac53e59c4b2b7dfe711d03a26
卢建生 <lujiansheng@bigbaser.com>
2018-05-02 11:41:10 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@ require('./style/index.less');
 require('./style/style.css');
 import { x } from './test';
  
+let submitFile = false;//是否提交过资料
+let formPage = $('#form-page');
+let successPage = $("#success-page");
+
 $(document).ready(function () {
-    $('body').html('<p class="p">testddsfdsfsdf</p>')
+    //$('body').html('<p class="p">testddsfdsfsdf</p>')
+    if(submitFile){
+        successPage.show();
+    }else{
+        successPage.hide();
+    }
     $("p").click(function () {
         alert(x);
         $(this).hide();
         setTimeout(() => {
             $(this).show();
         }, 2000)
+
+        
     });
 });
